ca4a92f3205c1e756948689b7a68f368c5f0ffdb
Markus Ressel <mail@markusressel.de>
2019-12-03 04:45:33 +0100

    split install and run parts to 'venv-install' and 'venv-run', added 'venv-update', improved console output, exit venv earlier

--- install.sh
+++ install.sh
@@ -2,5 +2,11 @@
 sudo cp venv-install /usr/bin/
 sudo chmod +x /usr/bin/venv-install
 
+sudo cp venv-run /usr/bin/
+sudo chmod +x /usr/bin/venv-run
+
 sudo cp venv-uninstall /usr/bin/
 sudo chmod +x /usr/bin/venv-uninstall
+
+sudo cp venv-update /usr/bin/
+sudo chmod +x /usr/bin/venv-update
